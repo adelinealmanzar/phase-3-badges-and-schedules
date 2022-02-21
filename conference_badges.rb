849645d3d0c40865391415c8f1802b2e3886af14
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-02-20 16:55:44 -0800

    commit tree fa01da4ccde2274eba500198479852aea6250e14

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator attendees
+    attendees.map do |attendee|
+        badge_maker(attendee)
+    end
+end
+
+def assign_rooms speakers
+    new_arr = speakers.map do |speaker|
+        "Hello, #{speaker}! You'll be assigned to room #{speakers.index("#{speaker }") + 1}!"
+    end
+    new_arr
+end
+
+def printer(arr)
+    batch_badge_creator(arr).each do |el|
+        puts el
+    end
+    assign_rooms(arr).each do |el|
+        puts el
+    end
+end
